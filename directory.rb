4acd1532f8df87e1674da6be0c36c272f190da8f
YohannTisserand <tisserand.yohann@gmail.com>
2021-03-18 13:42:42 +0000

    add load student

--- directory.rb
+++ directory.rb
@@ -1,13 +1,22 @@
 @students = []
 
-def save_student
+def save_students
   #open the file for writing
-  file = File.open("student.csv", "w")
+  file = File.open("students.csv", "w")
   # iterate over the array of student
   @students.each do |student|
     student_data = [student[:name], student[:cohort]]
     csv_line = student_data.join(", ")
     file.puts csv_line
+  end
+  file.close
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(",")
+      @students << {name: name, cohort: cohort.to_sym}
   end
   file.close
 end
@@ -23,6 +32,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 
@@ -33,7 +43,9 @@ def process(selection)
   when "2"
     show_students
   when "3"
-    save_student
+    save_students
+  when "4"
+    load_students
   when "9"
     exit
   else
