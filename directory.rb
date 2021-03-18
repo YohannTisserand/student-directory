dd7ec3009ecdf6ba7e1f66596b95dd177b82aa65
YohannTisserand <tisserand.yohann@gmail.com>
2021-03-18 11:44:12 +0000

    refactoring again

--- directory.rb
+++ directory.rb
@@ -1,18 +1,45 @@
+@students = []
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else
+    "Invalid command. Try again."
+  end
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an empty array -->
-  students = []
-  # get the name -->
   name = gets.chomp
-  # while the name is not empty, repeat -->
   while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
-    # get another name from the user -->
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
     name = gets.chomp
   end
-  students
 end
 
 def print_header
@@ -20,15 +47,12 @@ def print_header
   puts "-------------"
 end
 
-def print(students)
-  students.each { |student| puts "#{student[:name]} (#{student[:cohort]} cohort)" }
+def print_students_list
+  @students.each.with_index(1) { |student, idx| puts "#{idx}. #{student[:name]} (#{student[:cohort]} cohort)" }
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+interactive_menu
