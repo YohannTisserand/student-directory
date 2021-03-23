b0326ec62e8de37ef2bc497369aa69c55d114ebe
YohannTisserand <tisserand.yohann@gmail.com>
2021-03-23 16:37:01 +0000

    exercice 2 solved

---
+++ exercises/14_1_to_8.rb
@@ -0,0 +1,106 @@
+@students = []
+@default_file = "students.csv"
+
+def save_students
+  #open the file for writing
+  file = File.open(@default_file, "w")
+  # iterate over the array of student
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(", ")
+    file.puts csv_line
+  end
+  file.close
+end
+
+def load_students(filename = @default_file)
+  file = File.open(filename, "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(",")
+    add_students(name, cohort)
+  end
+  file.close
+end
+
+def add_students(name, cohort)
+  @students << {name: name, cohort: cohort.to_sym}
+end
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  name = STDIN.gets.chomp
+  cohort = STDIN.gets.chomp
+  while !name.empty? do
+    add_students(name, cohort)
+    puts "Now we have #{@students.count} students"
+    name = STDIN.gets.chomp
+    cohort = STDIN.gets.chomp
+  end
+end
+
+def try_load_students
+  filename = ARGV.first
+  return if filename.nil?
+  if File.exists?(filename)
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else
+    puts "Sorry, #{filename} doesn't exist."
+    exit
+  end
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(STDIN.gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
+  puts "9. Exit"
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "3"
+    save_students
+  when "4"
+    load_students
+  when "9"
+    exit
+  else
+    "Invalid command. Try again."
+  end
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print_students_list
+  @students.each.with_index(1) { |student, idx| puts "#{idx}. #{student[:name]} (#{student[:cohort]} cohort)" }
+end
+
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
+end
+
+try_load_students
+interactive_menu
